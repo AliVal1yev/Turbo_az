8a5e8db148dce5d9246687762f5387fb573d64f9
Valiyev <ali.valiyev@bp.com>
2024-07-03 14:02:29 +0400

    test

--- turbo/advertisement/models.py
+++ turbo/advertisement/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Advertisement(models.Model):
     name = models.CharField(max_length=32)
     model = models.CharField(max_length=32)
