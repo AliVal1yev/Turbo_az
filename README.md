61692ddba7209070a6d7a272671727ab583ccffb
ali <alivaliyev@gmail.com>
2024-08-26 13:03:27 +0400

    settings changes

--- README.md
+++
@@ -1,7 +0,0 @@
-If you want to sent an email, you must enter your email an password settings.py
-EMAIL_HOST_USER = 'Your email'
-EMAIL_HOST_PASSWORD = 'Your password'
-And when you add new advertisement in your project email sent automatically form your HOST email.
-
-
-
--- turbo/turbo/settings.py
+++ turbo/turbo/settings.py
@@ -167,8 +167,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # settings.py
 
-CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
-CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
+CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
